86518c7d2f68e8190377f53eeda573ce474236ab
datweb07 <dat82770@gmail.com>
2026-02-25 23:13:45 +0700

    chore: add base configuration in Config.php

--- config.php
+++ config.php
@@ -2,4 +2,19 @@
 
 const _AUTH = true;
 
+const _HOST = 'localhost';
+const _DB = 'db_manager';
+const _USER = 'root';
+const _PASS = '123456';
+const _DRIVER = 'mysql';
+
+const _DEBUG = true;        // debug lỗi
+
+// thiết lập host
+define('_HOST_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/manager_php');
+define('_HOST_URL_TEMPLATES', 'http://' . $_SERVER['HTTP_HOST'] . '/manager_php/templates');
+
+// thiết lập path
+define('_PATH_URL', __DIR__);
+define('_PATH_URL_TEMPLATES', __DIR__ . '/templates');
 ?>
