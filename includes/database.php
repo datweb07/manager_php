74dae9bb7baf43a3f45c8a5b08671749349089b4
Đạt Trương <dat82770@gmail.com>
2026-02-26 15:43:07 +0700

    feat: implement select query function (#6)

--- includes/database.php
+++ includes/database.php
@@ -1,8 +1,32 @@
 <?php
 
-if (!defined('_AUTH')){
+if (!defined('_AUTH')) {
     die('Truy cập không hợp lệ');
 }
 
+// truy vấn select *
+function get_All($sql)
+{
+    global $conn;
+
+    $stm = $conn->prepare($sql);
+
+    $stm->execute();
+
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function get_One($sql)
+{
+    global $conn;
+
+    $stm = $conn->prepare($sql);
+
+    $stm->execute();
+
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
 
 ?>
