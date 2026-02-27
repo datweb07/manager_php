157be5f83756c4a441ed6ccf10fea2c46c9b55df
datweb07 <dat82770@gmail.com>
2026-02-27 22:08:59 +0700

    feat: implement update query function

--- includes/database.php
+++ includes/database.php
@@ -4,7 +4,7 @@ if (!defined('_AUTH')) {
     die('Truy cập không hợp lệ');
 }
 
-// truy vấn select *
+// truy vấn data
 function get_All($sql)
 {
     global $conn;
@@ -39,13 +39,43 @@ function insert_data($table, $data)
     $column = implode(',', $keys);
     $place_holder = ':' . implode(',:', $keys);
 
-    
+
     $sql = "INSERT INTO $table ($column) VALUES ($place_holder)";        // :name: placeholder
     echo $sql;
     $stm = $conn->prepare($sql);      // bảo vệ khỏi tấn công như SQL Injection
 
 
     // thực thi 
+    $stm->execute($data);
+}
+
+
+
+// update data
+function update_data($table, $data, $condition = '')
+{
+    global $conn;
+    $update = '';
+
+    foreach($data as $key => $value){
+        $update .= $key . ' = :' . $key . ', ';
+    }
+
+    $update = rtrim($update, ', ');
+
+    echo $update;
+
+    if (!empty($condition)){
+        $sql = "UPDATE $table SET $update WHERE $condition";
+    }
+    else {
+        $sql = "UPDATE $table SET $update";
+    }
+
+    $stm = $conn->prepare($sql);
+
+    
+
     $stm->execute($data);
 }
 
